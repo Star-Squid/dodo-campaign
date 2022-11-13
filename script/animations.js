f9cc5869726f3abe922c01ce6d3cc22a444ce81c
Barbara <81830542+Star-Squid@users.noreply.github.com>
2022-11-13 13:04:29 +0000

    initial commit

---
+++ script/animations.js
@@ -0,0 +1,9 @@
+function showClippy() { 
+	document.getElementById("clippy-question").style.display="none"; 
+	document.getElementById("clippy").style.display="flex"; 
+}
+
+function hideClippy() { 
+	document.getElementById("clippy-question").style.display="grid"; 
+	document.getElementById("clippy").style.display="none"; 
+}
